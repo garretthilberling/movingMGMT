f1331f6548206c249d387b297583219cd48e5045
garretthilberling <ghilberling@knights.ucf.edu>
2022-08-17 23:44:28 -0400

    initial commit

---
+++ server/moving-mgmt-api/src/test/java/com/moving_mgmt/movingmgmtapi/MovingMgmtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moving_mgmt.movingmgmtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovingMgmtApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
