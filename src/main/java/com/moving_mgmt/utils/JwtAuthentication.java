ae4783ebb396c8a9f2e1583720af8a5cc1a5ffc4
garretthilberling <ghilberling@knights.ucf.edu>
2022-09-01 01:20:59 -0400

    set up secret key in a safer way, ensuring the key is on a .env file.

--- src/main/java/com/moving_mgmt/utils/JwtAuthentication.java
+++ src/main/java/com/moving_mgmt/utils/JwtAuthentication.java
@@ -1,21 +1,26 @@
 package com.moving_mgmt.utils;
 
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
 import java.security.Key;
+import java.security.Provider;
+import java.security.Security;
+import java.util.Base64;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import io.jsonwebtoken.*;
-
-import java.util.Date;
-
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.security.Keys;
 
+import javax.crypto.spec.SecretKeySpec;
+
 public class JwtAuthentication {
-    private static Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static Dotenv dotenv = Dotenv.load();
+    private static String secret = dotenv.get("SECRET");
+    private static byte[] decodeKey = Base64.getDecoder().decode(secret);
+    private static Key key = new SecretKeySpec(decodeKey,0,decodeKey.length, "HMACSHA256");
 
     public static String createJWT(String subject) {
+        System.out.println("KEY: " + Base64.getEncoder().encodeToString(key.getEncoded()));
         return Jwts.builder().setSubject(subject).signWith(key).compact();
     }
 
